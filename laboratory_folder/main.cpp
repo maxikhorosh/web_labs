3738502f6eedd6c6874129b906cd7562ff3c06bc
maxikhorosh <ghettogopnik1703@gmail.com>
2018-09-30 17:25:27 +0300

    add_addition

--- laboratory_folder/main.cpp
+++ laboratory_folder/main.cpp
@@ -1,8 +1,17 @@
-#include <cstdio>
+#include <iostream>
+#include <locale>
 #include "Windows.h"
 
+using namespace std;
 int main()
 {
+    double a,b;
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251");
+    system("cls");
 
+    cout << "Введите числа для суммирования: ";
+    cin >> a >> b;
+    cout << "Сумма равна: " << a + b << endl;
     return 0;
 }
